4f7093fe290e3d9919f6d3986dfcf82e805e3c2b
Hyman_Chen <hyman_chen@wistron.com>
2015-11-12 13:53:51 +0800

    first commit initial files

---
+++ src/main/java/io/hyman/Testserver2Application.java
@@ -0,0 +1,12 @@
+package io.hyman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testserver2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testserver2Application.class, args);
+    }
+}
